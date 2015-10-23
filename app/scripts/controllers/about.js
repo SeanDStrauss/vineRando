a83b7241798bcdb85c417bc888a4ed12f6558b72
Sean Strauss <seandstrauss@gmail.com>
2015-10-23 19:56:28 -0400

    first commmit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vineProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vineProjectApp
+ */
+angular.module('vineProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vineProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vineProjectApp
+ */
+angular.module('vineProjectApp')
+  .controller('MainCtrl', function ($scope, $http, $sce) {
+   var vines = {};
+   $http.defaults.headers.common["X-Mashape-Authorization"] = "zEs3Gj1vuFmshm446Z3SmHLJBBadp11x51Kjsnj0lZfKUUlDkx";
+	$http({
+		method: 'GET',
+		url: 'https://community-vineapp.p.mashape.com/timelines/popular'
+
+	}).then(function successCallback(response) {
+		// this callback will be called asynchronously
+		// when the response is available
+		vines = response.data.data.records;
+		console.log(vines);
+
+		var randomNum = Math.floor((Math.random() * 18) + 1);
+
+		$scope.trustSrc = function(src){
+			return $sce.trustAsResourceUrl(src);
+		}
+
+		$scope.randomVine = vines[randomNum];
+
+		
+		
+
+		//console.log(response);
+	});
+  });
+
+
+
+ 
+	
